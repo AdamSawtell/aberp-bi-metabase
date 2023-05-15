08ab6b4b746c01610b8b5054c286662d1c5e4c57
Adam Sawtell <110363956+AdamSawtell@users.noreply.github.com>
2023-05-15 14:19:21 +0930

    Create utils.sql

---
+++ install/utils.sql
@@ -0,0 +1,44 @@
+
+-- command to execute this file:
+-- sudo -u postgres psql -d metabase -f utils.sql
+
+update metabase_table 
+set visibility_type = 'cruft'
+where db_id in (select id from metabase_database where is_sample = 'no')
+;
+
+update metabase_table 
+set visibility_type = ''
+where name like 'bi%cache%'
+and db_id in (select id from metabase_database where is_sample = 'no')
+;
+
+update metabase_table
+set display_name = replace(display_name,' Cache', '')
+where name like '%cache'
+;
+
+update metabase_table
+set display_name = replace(display_name,'Bi ', '')
+where name like 'bi%'
+;
+
+update metabase_table
+set display_name = 'Unit of Measure'
+where name like 'bi_uom%'
+;
+
+update metabase_table
+set display_name = 'Business Partner'
+where name like 'bi_bpartner%'
+;
+
+update metabase_table
+set display_name = 'Ship/Receipt'
+where name like 'bi_inout%'
+;
+
+update metabase_table
+set display_name = 'Ship/Receipt Line'
+where name like 'bi_inout_line%'
+;
